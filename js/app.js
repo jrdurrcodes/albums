e5949a391e8da51bbee86900703b8a87b969d1d1
jrdurrcodes <biycoachn@yahoo.com>
2025-02-14 14:50:46 -0600

    build an album in backend

---
+++ js/app.js
@@ -0,0 +1,74 @@
+class music {
+
+    constructor () {
+        this.data = [ 
+            {
+                artist: '',
+                title: '',
+                genre: '',
+                albumYear: '',
+                label: '',
+                isFavorite: false
+            },
+            {
+                artist: '',
+                title: '',
+                genre: '',
+                albumYear: '',
+                label: '',
+                isFavorite: false
+            },
+            {
+                artist: '',
+                title: '',
+                genre: '',
+                albumYear: '',
+                label: '',
+                isFavorite: false
+            },
+            {
+                artist: '',
+                title: '',
+                genre: '',
+                albumYear: '',
+                label: '',
+                isFavorite: false
+            },
+            {
+                artist: '',
+                title: '',
+                genre: '',
+                albumYear: '',
+                label: '',
+                isFavorite: false
+            },
+            {
+                artist: '',
+                title: '',
+                genre: '',
+                albumYear: '',
+                label: '',
+                isFavorite: false
+            },
+                       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ]
+    }
+}
