8f75ca3643b38de6170a2eac72edbb7c0175f963
jrdurrcodes <biycoachn@yahoo.com>
2025-02-14 15:13:28 -0600

    building an albums page

--- js/app.js
+++ js/app.js
@@ -3,19 +3,21 @@ class music {
     constructor () {
         this.data = [ 
             {
-                artist: '',
-                title: '',
-                genre: '',
-                albumYear: '',
-                label: '',
+                artist: 'Jon B.',
+                title: 'Cool Relax',
+                genre: 'R & B',
+                albumRecorded:'1996',
+                albumReleased: '1997',
+                label: 'Yab Yum',
                 isFavorite: false
             },
             {
-                artist: '',
-                title: '',
-                genre: '',
-                albumYear: '',
-                label: '',
+                artist: 'Monica',
+                title: 'After the Rain',
+                genre: 'R & B',
+                albumRecorded: '2003',
+                albumRelease: '2003',
+                label: 'J',
                 isFavorite: false
             },
             {
